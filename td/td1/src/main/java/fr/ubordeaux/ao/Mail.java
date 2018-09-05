5605e38c176e9f2fd1053789e3131105260d5036
xblanc33 <blancxav@gmail.com>
2018-09-05 15:43:09 +0200

    equals in mail

--- td/td1/src/main/java/fr/ubordeaux/ao/Mail.java
+++ td/td1/src/main/java/fr/ubordeaux/ao/Mail.java
@@ -1,5 +1,7 @@
 package fr.ubordeaux.ao;
 
+import java.util.Objects;
+
 public class Mail {
     private String mail;
 
@@ -10,4 +12,22 @@ public class Mail {
     public String getMail() {
         return mail;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (! (other instanceof Mail)) return false;
+        Mail otherMail = (Mail) other;
+        return  this.mail.compareTo(otherMail.getMail())==0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mail);
+    }
+
+    @Override
+    public String toString() {
+        return "mails: "+mail;
+    }
+
 }
